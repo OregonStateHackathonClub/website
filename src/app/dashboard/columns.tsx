7e84c660486306745002c99530bf89ddb1ebde05
owenkrause <owenjkrause@gmail.com>
2024-11-16 14:15:20 -0800

    add filter, selection, and bulk download to resume book

--- src/app/dashboard/columns.tsx
+++ src/app/dashboard/columns.tsx
@@ -2,7 +2,8 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Button } from "@/components/ui/button"
-import { File } from "lucide-react"
+import { Checkbox } from "@/components/ui/checkbox"
+import { ArrowUpDown, File } from "lucide-react"
 
 export type Application = {
   createdAt: Date
@@ -18,7 +19,7 @@ const handleDownload = async(path: string) => {
   const res = await fetch(`/api/download/${path}`, { method: "GET" })
   const blob = await res.blob()
   const url = window.URL.createObjectURL(blob);
-  const link = document.createElement('a')
+  const link = document.createElement("a")
   link.href = url
   link.download = path
   link.click()
@@ -27,12 +28,45 @@ const handleDownload = async(path: string) => {
 
 export const columns: ColumnDef<Application>[] = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "user.name",
     header: "Name",
   },
   {
+    id: "email",
     accessorKey: "user.email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc" )}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    }
   },
   {
     accessorKey: "university",
