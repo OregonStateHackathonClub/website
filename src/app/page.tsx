dfa3705d82db2e364531246d9d419bc8b5fa6072
owenkrause <owenjkrause@gmail.com>
2025-01-16 00:03:14 -0800

    feat: faq section accordian

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,19 @@
 
 import { useRef } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-import { GridAnimation } from "@/components/animation";
 import { ChevronButton } from "@/components/chevron";
 import { Navbar } from "@/components/navbar";
 import { Countdown } from "@/components/countdown";
 
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 
 const Home = () => {
 
@@ -27,60 +26,62 @@ const Home = () => {
     <>
       <Navbar aboutRef={about} sponsorsRef={sponsors} faqRef={faq}/>
       <div className="w-screen h-screen flex items-center justify-center">
-        <GridAnimation />
         <ChevronButton targetRef={about}/>
       </div>
       <div ref={about} className="w-screen h-screen flex flex-col items-center justify-center">
         <span>Oregon State&apos;s Biggest Hackathon</span>
-        <Countdown targetDate={new Date(1732327200 * 1000)} />
+        <Countdown targetDate={new Date(1743807600 * 1000)} />
       </div>
       <div ref={sponsors} className="w-screen h-screen flex flex-col gap-8 items-center justify-center">
         <span>Sponsors</span>
         <div className="flex flex-wrap gap-12 w-screen justify-center">
-          <Card className="w-[300px]">
-            <CardHeader>
-              <CardTitle>Sponsor name</CardTitle>
-              <CardDescription>Sponsor description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>(Sponsor img)</p>
-            </CardContent>
-            <CardFooter>
-              <p>Sponsor footer</p>
-            </CardFooter>
-          </Card>
-          <Card className="w-[300px]">
-            <CardHeader>
-              <CardTitle>Sponsor name</CardTitle>
-              <CardDescription>Sponsor description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Sponsor content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Sponsor footer</p>
-            </CardFooter>
-          </Card>
-          <Card className="w-[300px]">
-            <CardHeader>
-              <CardTitle>Sponsor name</CardTitle>
-              <CardDescription>Sponsor description</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>Sponsor content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Sponsor footer</p>
-            </CardFooter>
-          </Card>
+          <Link className="flex items-center" href="https://groq.com/" target="_blank">
+            <Image 
+              src="/images/groq_white.png"
+              width={256}
+              height={256}
+              alt="Groq logo"
+            />
+          </Link>
+          <Link className="flex items-center" href="https://sui.io/" target="_blank">
+            <Image 
+              src="/images/sui_sea.png"
+              width={256}
+              height={256}
+              alt="Groq logo"
+            />
+          </Link>
         </div>
-        <div className="flex flex-col gap-4">
-          <Link href="/sponsors">Interested in becoming a sponsor?</Link>
-          <Link href="/dashboard">Already a sponsor?</Link>
-        </div>
+        <p>Interested in becoming a sponsor? Contact us at sponsor@beaverhacks.org.</p>
       </div>
-      <div ref={faq} className="w-screen h-screen flex flex-col items-center justify-center">
-        <span>FAQ</span>
+      <div ref={faq} className="w-screen h-screen flex flex-col items-center">
+        <h1 className="text-2xl w-1/2 py-8">FAQs</h1>
+        <Accordion type="multiple" className="w-1/2">
+          <AccordionItem value="item-1" className="border-none">
+            <AccordionTrigger className="hover:no-underline text-lg">What is a Hackathon?</AccordionTrigger>
+            <AccordionContent>
+              A competition where teams build innovative projects from scratch. Network with fellow developers, learn new skills, and compete for prizes. 
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2" className="border-none">
+            <AccordionTrigger className="hover:no-underline text-lg">What is BeaverHacks?</AccordionTrigger>
+            <AccordionContent>
+              BeaverHacks is the official Hackathon hosted by the Hackathon Club at Oregon State University.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3" className="border-none">
+            <AccordionTrigger className="hover:no-underline text-lg">Who can come?</AccordionTrigger>
+            <AccordionContent>
+              Anyone currently enrolled in a college over the age of 18.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4" className="border-none">
+            <AccordionTrigger className="hover:no-underline text-lg">What if I don't have a team?</AccordionTrigger>
+            <AccordionContent>
+              Meet potential teammates during team formation at the start of the event. We'll help match you with others based on skills and interests.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>  
       </div>
     </>
   );
