dcc1906023af64ce61ad6ef290d5bf2a89f5b1fd
owenkrause <owenjkrause@gmail.com>
2025-11-04 15:13:32 -0800

    fix build errs

--- apps/judge/src/app/unauthorized.tsx
+++ apps/judge/src/app/unauthorized.tsx
@@ -7,9 +7,9 @@ export default function Unauthorized() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center bg-neutral-950 text-white">
 			<p>You must be a judge, admin, or superadmin to access this interface</p>
-			<p>If you're a judge/admin, talk to your organizers to obtain access</p>
+			<p>If you&apos;re a judge/admin, talk to your organizers to obtain access</p>
 			<p>
-				If you're configuring this application, ensure that you whitelist an
+				If you&apos;re configuring this application, ensure that you whitelist an
 				account as a superadmin. See the setup guide for more info
 			</p>
 			<Button>
