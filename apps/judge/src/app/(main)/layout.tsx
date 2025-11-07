430ee65f0093f2628a7f66d7149985132c9328da
Brandon Gill <157265645+BGill8@users.noreply.github.com>
2025-11-07 01:37:17 -0800

    changed aesthetics of manage team page

--- apps/judge/src/app/(main)/layout.tsx
+++ apps/judge/src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <main className="flex min-h-dvh flex-col bg-neutral-900 text-neutral-200">
+        <main className="flex min-h-dvh flex-col bg-neutral-900 text-neutral-200 pt-12">
             <Navbar />
             <div className="flex grow flex-col">{children}</div>
         </main>
