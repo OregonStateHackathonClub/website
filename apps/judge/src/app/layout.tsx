88e7b0883ada6bc695a634a4a12c9cd6624fb01a
Brandon Gill <157265645+BGill8@users.noreply.github.com>
2025-11-06 18:24:38 -0800

    fixed layout to make all dark mode

--- apps/judge/src/app/layout.tsx
+++ apps/judge/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="dark">
             <body 
               className={`${GeistSans.className} min-h-screen bg-neutral-950 text-neutral-200`}>
                 {children}
