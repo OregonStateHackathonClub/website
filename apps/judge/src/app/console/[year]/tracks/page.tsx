51d37f81fb2cb756d1303d5df0274ca32b3f483f
Stanley Eng <stanleyeng665@gmail.com>
2025-12-14 02:25:24 -0800

    Created basic layout of admin tracks page, can view tracks but need to add functionality for creating a track

--- apps/judge/src/app/console/[year]/tracks/page.tsx
+++ apps/judge/src/app/console/[year]/tracks/page.tsx
@@ -1,6 +1,105 @@
 // admin only
 // see the tracks, create new tracks, add rubrics to tracks (each track can have one rubric)
+import { Table, TableBody, TableCaption,TableCell, TableHead, TableHeader, TableRow, } from "@repo/ui/components/table"
+import { Navbar } from "@/components/navbar";
+import { Button } from "@repo/ui/components/button"
+import { Label } from "@repo/ui/components/label"
+import { Input } from "@repo/ui/components/input"
 
-export default function Page() {
-	return "";
+import { Dialog, 
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger, } from "@repo/ui/components/dialog";
+import { prisma } from "@repo/database";
+
+interface Track {
+	id: string;
+	name: string;
+	description: string;
+	prize: string | null;
+}
+
+export default async function Page(props: {
+	params: Promise<{ year: string }>;
+}) {
+	const params = await props.params;
+	const yearParam = params.year;
+
+	const tracks = await prisma.track.findMany({
+		where: { hackathonId: yearParam },
+		select: {
+			id: true,
+			name: true,
+			description: true,
+			prize: true,
+		}
+	});
+	return (
+		<div className="flex min-h-screen w-full flex-col bg-white">
+			<Navbar />
+			<div className="flex-1 p-10 text-black">
+				<h1 className="mb-6 text-center font-bold text-4xl text-black-900">
+					Tracks
+				</h1>
+				<Table>
+					<TableCaption>Create or view current tracks, and add rubrics to tracks.</TableCaption>
+					<TableHeader>
+						<TableRow>
+						<TableHead className="w-[200px]">Name</TableHead>
+						<TableHead>Description</TableHead>
+						<TableHead className="w-[150px]">Prize</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{tracks.map((track) => (
+							<TableRow key={track.id}>
+								<TableCell className="font-medium">{track.name}</TableCell>
+								<TableCell>{track.description}</TableCell>
+								<TableCell>{track.prize || 'N/A'}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+				<Dialog>
+					<form>
+						<DialogTrigger asChild>
+						<Button variant="outline">Create a track</Button>
+						</DialogTrigger>
+						<DialogContent className="sm:max-w-[425px]">
+						<DialogHeader>
+							<DialogTitle>Create a track</DialogTitle>
+							<DialogDescription>
+							Design your own track. Save when done.
+							</DialogDescription>
+						</DialogHeader>
+						<div className="grid gap-4">
+							<div className="grid gap-3">
+							<Label htmlFor="name-1">Name</Label>
+							<Input id="name-1" name="name"/>
+							</div>
+							<div className="grid gap-3">
+							<Label htmlFor="description-1">Description</Label>
+							<Input id="description-1" name="description"/>
+							</div>
+							<div className="grid gap-3">
+							<Label htmlFor="prize-1">Prize</Label>
+							<Input id="prize-1" name="prize"/>
+							</div>
+						</div>
+						<DialogFooter>
+							<DialogClose asChild>
+							<Button variant="outline">Cancel</Button>
+							</DialogClose>
+							<Button type="submit">Save changes</Button>
+						</DialogFooter>
+						</DialogContent>
+					</form>
+					</Dialog>
+			</div>
+		</div>
+	);
 }
--- apps/judge/src/components/navbar.tsx
+++ apps/judge/src/components/navbar.tsx
@@ -5,30 +5,30 @@ import { getCurrentHackathonId } from "@/lib/queries";
 import LoginLogoutButton from "./loginLogoutButton";
 
 export const Navbar = () => {
-	return (
-		<div className="flex h-20 items-center justify-between bg-gray-100 px-8">
-			<Suspense fallback={<HackathonHomepageButton />}>
-				<HackathonHomepageButtonWrapper />
-			</Suspense>
-			<LoginLogoutButton />
-		</div>
-	);
+  return (
+    <div className="flex h-20 items-center justify-between bg-gray-100 px-8 text-black">
+      <Suspense fallback={<HackathonHomepageButton />}>
+        <HackathonHomepageButtonWrapper />
+      </Suspense>
+      <LoginLogoutButton />
+    </div>
+  );
 };
 
 const HackathonHomepageButtonWrapper = async () => {
-	const currentHackathonId = await getCurrentHackathonId();
+  const currentHackathonId = await getCurrentHackathonId();
 
-	return <HackathonHomepageButton link={`/${currentHackathonId}`} />;
+  return <HackathonHomepageButton link={`/${currentHackathonId}`} />;
 };
 
 const HackathonHomepageButton = ({ link = "/" }: { link?: string }) => (
-	<Link
-		href={{ pathname: link }}
-		className="flex items-center gap-5 p-5 hover:bg-gray-200"
-	>
-		<div className="relative h-10 w-10">
-			<Image src="/beaver.png" alt="beaver" fill />
-		</div>
-		<h1>BeaverHacks Official Judging Platform</h1>
-	</Link>
+  <Link
+    href={{ pathname: link }}
+    className="flex items-center gap-5 p-5 hover:bg-gray-200"
+  >
+    <div className="relative h-10 w-10">
+      <Image src="/beaver.png" alt="beaver" fill />
+    </div>
+    <h1>BeaverHacks Official Judging Platform</h1>
+  </Link>
 );
