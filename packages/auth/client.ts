fdd896a051b4affd624e0e9b9d74c7a6ddd0b8df
pilijara <120620406+pilijara@users.noreply.github.com>
2025-12-19 13:30:09 -0800

    Logout auto refreshes to home page, and testing if preview now works

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
-    : "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL 
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}` 
+  : process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL 
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}` 
+  : "http://localhost:3000",
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
